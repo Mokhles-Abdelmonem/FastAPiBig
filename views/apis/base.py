138fc7c319d559f18a2788acee28be2adaec8293
Mokhles-Abdelmonem <mokhlesabdelmonem2@gmail.com>
2025-02-26 00:22:00 +0200

    <Change: clean and code enhancement >

--- views/apis/base.py
+++ views/apis/base.py
@@ -1,14 +1,13 @@
-from typing import List, Type, Optional, ClassVar, Dict
+from typing import List, Type, Optional, Dict
 from fastapi import APIRouter
 from pydantic import BaseModel
-from typing import List, Type, Optional, ClassVar, get_type_hints
 
 
-class BaseView:
+class BaseAPIView:
     schema_in: Optional[Type[BaseModel]] = None
     schema_out: Optional[Type[BaseModel]] = None
-    schemas_in: Optional[Dict[str, BaseModel]] = {}
-    schemas_out: Optional[Dict[str, BaseModel]] = {}
+    schemas_in: Dict[str, Type[BaseModel]] = {}
+    schemas_out: Dict[str, Type[BaseModel]] = {}
 
     model: Optional[Type] = None
     methods: List[str] = []
@@ -28,82 +27,88 @@ class BaseView:
         "partial_update",
     ]
 
-    def _get_schema_in_class(self, method: str = None):
-        return self.schemas_in.get(method, self.schema_in)
-
-    def _get_schema_out_class(self, method: str = None):
-        return self.schemas_out.get(method, self.schema_out)
-
-    @classmethod
-    def as_router(cls: Type["APIView"], prefix: str, tags: list = None) -> APIRouter:
-        router = APIRouter(prefix=prefix, tags=tags)
-
-        # Create an instance of the view class
-        instance = cls()
-
+    def __init__(self, router: Optional[APIRouter] = None):
         class Wrapper:
             pass
 
-        def load_method(method: str, set_annotations=False):
-            attr = getattr(instance, method)
-            setattr(Wrapper, method, attr)
-            if set_annotations:
-                attr.__annotations__["data"] = instance._get_schema_out_class(method)
+        self.wrapper = Wrapper
+        self.router = router or APIRouter()
+        self.load_base_methods()
+        self.load_post_methods()
 
-        def load_api(method):
-            router.post(
-                f"/{method}",
-                response_model=instance._get_schema_out_class(method),
-                name=method,
-            )(getattr(Wrapper, method))
+    def _get_schema_in_class(self, method: str = None) -> Optional[Type[BaseModel]]:
+        return self.schemas_in.get(method, self.schema_in)
 
-        def load_post_methods():
-            for post_method in instance.post_methods:
-                load_method(post_method)
-                load_api(post_method)
+    def _get_schema_out_class(self, method: str = None) -> Optional[Type[BaseModel]]:
+        return self.schemas_out.get(method, self.schema_out)
 
-        def load_base_methods():
-            for method in instance.methods:
-                if method in instance.allowed_methods:
+    @classmethod
+    def as_router(cls: Type["BaseAPIView"], prefix: str, tags: Optional[List[str]] = None) -> APIRouter:
+        instance = cls(APIRouter(prefix=prefix, tags=tags))
+        return instance.router
 
-                    set_annotations = (
-                        True
-                        if method in ["create", "update", "partial_update"]
-                        else False
-                    )
-                    load_method(method, set_annotations=set_annotations)
+    def register_method_wrapper(self, method: str, set_annotations: bool = False):
+        attr = getattr(self, method, None)
+        if not attr:
+            raise AttributeError(f"Method '{method}' not found in {self.__class__.__name__}")
 
-                    # TODO : Clean
-                    #   separate each method to its own class
-                    schema_out = instance._get_schema_out_class(method)
-                    if method == "create":
-                        router.post("/", response_model=schema_out, name=method)(
-                            getattr(Wrapper, method)
-                        )
-                    elif method in ["update", "partial_update"]:
-                        router.put("/{pk}", response_model=schema_out, name=method)(
-                            getattr(Wrapper, method)
-                        )
-                    elif method == "get":
-                        router.get("/{pk}", response_model=schema_out, name=method)(
-                            getattr(Wrapper, method)
-                        )
-                    elif method == "list":
-                        router.get("/", response_model=List[schema_out], name=method)(
-                            getattr(Wrapper, method)
-                        )
-                    elif method == "delete":
-                        router.delete("/{pk}", name=method)(getattr(Wrapper, method))
+        setattr(self.wrapper, method, attr)
 
-        load_base_methods()
-        load_post_methods()
+        if set_annotations:
+            attr.__annotations__["data"] = self._get_schema_out_class(method)
 
-        return router
+    def _register_route(self, method_type: str, method_name: str, path: str):
+        if not hasattr(self.wrapper, method_name):
+            raise AttributeError(f"Method '{method_name}' not found in wrapper.")
+
+        route_method = getattr(self.router, method_type)
+        route_method(
+            path,
+            response_model=self._get_schema_out_class(method_name),
+            name=method_name,
+        )(getattr(self.wrapper, method_name))
+
+    def load_post(self, method: str, path: str = "/"):
+        self._register_route("post", method, path)
+
+    def load_get(self, method: str, path: str = "/{pk}"):
+        self._register_route("get", method, path)
+
+    def load_list(self, method: str, path: str = "/"):
+        self._register_route("get", method, path)
+
+    def load_put(self, method: str, path: str = "/{pk}"):
+        self._register_route("put", method, path)
+
+    def load_delete(self, method: str, path: str = "/{pk}"):
+        self._register_route("delete", method, path)
+
+    def load_post_methods(self):
+        for method in self.post_methods:
+            self.register_method_wrapper(method)
+            self.load_post(method=method, path=f"/{method}")
+
+    def load_base_methods(self):
+        for method in self.methods:
+            if method in self.allowed_methods:
+                set_annotations = method in ["create", "update", "partial_update"]
+                self.register_method_wrapper(method, set_annotations=set_annotations)
+
+                if method == "create":
+                    self.load_post(method)
+                elif method in ["update", "partial_update"]:
+                    self.load_put(method)
+                elif method == "get":
+                    self.load_get(method)
+                elif method == "list":
+                    self.load_list(method)
+                elif method == "delete":
+                    self.load_delete(method)
 
 
-class APIView(BaseView):
+class APIView(BaseAPIView):
 
-    async def create(self, data):
+    async def create(self, data: BaseModel):
         instance = await self.model.objects.create(**data.dict())
         return self.schema_out.model_validate(instance.__dict__)
 
@@ -111,19 +116,25 @@ class APIView(BaseView):
         instance = await self.model.objects.get(pk=pk)
         return self.schema_out.model_validate(instance.__dict__)
 
-    async def update(self, pk: int, data):
-        instance = await self.model.objects.save()
+    async def update(self, pk: int, data: BaseModel):
+        instance = await self.model.objects.get(pk=pk)
+        for key, value in data.dict().items():
+            setattr(instance, key, value)
+        await instance.save()
         return self.schema_out.model_validate(instance.__dict__)
 
     async def delete(self, pk: int):
-        return await self.model.objects.delete(pk)
+        instance = await self.model.objects.get(pk=pk)
+        await instance.delete()
+        return {"detail": "Deleted successfully"}
 
     async def list(self):
-        instances = await self.model.objects.filter()
-        return [
-            self.schema_out.model_validate(instance.__dict__) for instance in instances
-        ]
+        instances = await self.model.objects.all()
+        return [self.schema_out.model_validate(instance.__dict__) for instance in instances]
 
-    async def partial_update(self, pk: int, data):
-        instance = await self.model.objects.save()
+    async def partial_update(self, pk: int, data: BaseModel):
+        instance = await self.model.objects.get(pk=pk)
+        for key, value in data.dict(exclude_unset=True).items():
+            setattr(instance, key, value)
+        await instance.save()
         return self.schema_out.model_validate(instance.__dict__)
